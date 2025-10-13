006c1c6de49e7e39ef8d44b0b55cbc03219395c6
jay <jailavaniya010@gmail.com>
2025-10-13 20:42:58 +0530

    add exp. section

---
+++ sections/experience.js
@@ -0,0 +1,8 @@
+export async function showExperience(typeWriter) {
+  await typeWriter("\n💼  My Experience:");
+  await typeWriter("  - Member of Axios Technical Club - InfoSec @IIIT Lucknow");
+  await typeWriter("  - Achieved 32th Global Rank in Incognito CTF 6.0 - By IIIT Lucknow.");
+  await typeWriter("  - Secured 88th Global Rank in Road To CyberBay CTF - By MetaCTF.");
+  await typeWriter("  - Ended up at 98th Global Rank in MetaCTF July Flash CTF.");
+  await typeWriter("  - Achieved Global Rank of 440 on CTFLearn Platform.");
+}
