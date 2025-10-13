5736e4b78736f7f3a7f123688afa92f3e4e19e3f
jay <jailavaniya010@gmail.com>
2025-10-13 20:42:44 +0530

    add about section

---
+++ sections/about.js
@@ -0,0 +1,5 @@
+export async function showAbout(typeWriter) {
+  await typeWriter("\n👋 Hi, I’m Jai Parashar, a 2nd-year B.Tech student in Computer Science and Artificial Intelligence (CSAI)");
+  await typeWriter("😊 I currently hold a CGPA of 9.3 and enjoy applying my knowledge to real-world problems in development and cybersecurity.");
+  await typeWriter("🚀 Always learning new technologies and improving my skills.");
+}
