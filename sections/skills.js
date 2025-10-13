67c5e408cabed132907a3f413c3bca727e85008a
jay <jailavaniya010@gmail.com>
2025-10-13 20:43:59 +0530

    add skills section

---
+++ sections/skills.js
@@ -0,0 +1,12 @@
+export async function showSkills(typeWriter) {
+  await typeWriter("\n⚙️  My Core Skills:");
+  await typeWriter("  - C/C++, Java, Python, JavaScript, PHP, SQL, Bash");
+  await typeWriter("  - MongoDB, Express.js, React.js, Node.js (MERN Stack)");
+  await typeWriter("  - Tailwind CSS, Bootstrap, Spring Boot, NumPy, Pandas, TypeScript");
+  await typeWriter("  - NMAP, Wireshark, Metasploit, Burp Suite, OWASP ZAP, John the Ripper");
+  await typeWriter("  - NetCat, SQLMap, Hydra, StegSolve, Ghidra, Autopsy, Aircrack-ng");
+  await typeWriter("  - Git, GitHub, Docker, Kali Linux, Postman, Appwrite");
+  await typeWriter("\n🦾  My Soft Skills:");
+  await typeWriter("  - Critical Thinking, Public Speaking, Team Management");
+  await typeWriter("  - Problem-Solving, Adaptability, Time Management, Event Planning");
+}
