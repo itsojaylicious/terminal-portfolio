a50539620fe333cc68c9a826bdfbbfabac036c82
jay <jailavaniya010@gmail.com>
2025-10-13 20:43:23 +0530

    add contact

---
+++ sections/contact.js
@@ -0,0 +1,9 @@
+import open from 'open';
+
+export async function showContact(typeWriter) {
+  await typeWriter("\n📧 Email: jailavaniya010@gmail.com");
+  await typeWriter("☎️ Phone: +91-9694281328");
+  await typeWriter("💼 LinkedIn: @jaiparashar - Type 'linkedin' to open");
+  await typeWriter("🌐 Resume: (To be Updated)");
+  await typeWriter("🐦 GitHub: @itsojaylicious - Type 'github' to open");
+}
